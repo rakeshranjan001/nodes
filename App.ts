4d760be1b9901d4c368f2871d00675d518428bcc
Rakesh Ranjan <rranjan@GSHYD-C1MRQ3CBDTY3.local>
2019-04-12 16:35:32 +0530

    using ts-node

--- App.ts
+++ App.ts
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000
 const App = express();
 App.get('/',(req,res)=>{
   res.send({
-    message:"Home !"
+    message:"Home using ts-node!"
   })
 })
 App.listen(PORT)

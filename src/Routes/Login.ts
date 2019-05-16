5bfd2f686d1e58acee5c682d30638ffc0af39da9
gs-rranjan <rranjan@gainsight.com>
2019-05-16 14:31:31 +0530

    some login changes

--- src/Routes/Login.ts
+++ src/Routes/Login.ts
@@ -13,32 +13,32 @@ const db =  knex({
     }
 });
 
-login.post('/',(req,res)=>{
+login.post('/',(req: { body: { email: any; password: any; }; },res: { json: (arg0: any) => void; status: { (arg0: number): { json: (arg0: string) => void; }; (arg0: number): { json: (arg0: string) => void; }; (arg0: number): { json: (arg0: string) => void; }; }; })=>{
     db.select('email','hash').from('login').where('email','=',req.body.email)
-        .then(data => {
+        .then((data: { hash: any; }[]) => {
             const isValid = bcrypt.compareSync(req.body.password,data[0].hash);
             if(isValid){
                 return db.select('*').from('users').where('email','=',req.body.email)
-                    .then(user => {
+                    .then((user: any[]) => {
                         res.json(user[0])
                         //res.json('success')
                     })
-                    .catch(err => res.status(400).json('Unable to get user'))
+                    .catch((err: any) => res.status(400).json('Unable to get user'))
             }
             else{
                 res.status(400).json('Wrong Credentials')
             }
         })
-        .catch(err => res.status(400).json('Wrong Credentials'))
+        .catch((err: any) => res.status(400).json('Wrong Credentials'))
 })
 
 
 
-login.get('/profile/:id',(req,res)=>{
+login.get('/profile/:id',(req: { params: { id: any; }; },res: { json: (arg0: any) => void; status: { (arg0: number): { json: (arg0: string) => void; }; (arg0: number): { json: (arg0: string) => void; }; }; })=>{
     const{id} =req.params;
     let found = false;
     db.select('*').from('users').where({id}) // .where({ id : id })
-    .then(user =>{
+    .then((user: any[]) =>{
         if(user.length){
             res.json(user[0]);
         }
@@ -46,7 +46,7 @@ login.get('/profile/:id',(req,res)=>{
             res.status(400).json("Not Found")
         }
     })
-    .catch(err=>res.status(400).json('Not Found'))
+    .catch((err: any)=>res.status(400).json('Not Found'))
 
 })
 module.exports = login;
